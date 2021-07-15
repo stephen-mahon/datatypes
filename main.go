c561fbe10662fa877192e4bd05d653ec67895658
Stephen Mahon <stemahon@gmail.com>
2021-07-15 10:27:28 -0500

    Type definitions and Aliases

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 func main() {
 	p := organization.NewPerson("Steve", "Mahon")
 	err := p.SetTwitterHandler("@steve")
+	fmt.Printf("%T\n", organization.TwitterHandler("Test"))
 	if err != nil {
 		fmt.Printf("An error occurred setting twitter handler: %s\n", err)
 	}
 	fmt.Println(p.TwitterHandler())
+	fmt.Println(p.TwitterHandler().RedirectURL())
 	fmt.Println(p.ID())
 	fmt.Println(p.FullName())
 }
--- organization/person.go
+++ organization/person.go
@@ -6,6 +6,13 @@ import (
 	"strings"
 )
 
+type TwitterHandler string
+
+func (th TwitterHandler) RedirectURL() string {
+	cleanHandler := strings.TrimPrefix(string(th), "@")
+	return fmt.Sprintf("https://www.twitter.com/%s", cleanHandler)
+}
+
 type Identifiable interface {
 	ID() string
 }
@@ -13,7 +20,7 @@ type Identifiable interface {
 type Person struct {
 	firstName      string
 	lastName       string
-	twitterHandler string
+	twitterHandler TwitterHandler
 }
 
 func NewPerson(firstName, lastName string) Person {
@@ -31,10 +38,10 @@ func (p *Person) ID() string {
 	return "12345"
 }
 
-func (p *Person) SetTwitterHandler(handler string) error {
+func (p *Person) SetTwitterHandler(handler TwitterHandler) error {
 	if len(handler) == 0 {
 		p.twitterHandler = handler
-	} else if !strings.HasPrefix(handler, "@") {
+	} else if !strings.HasPrefix(string(handler), "@") {
 		return errors.New("twitter handler must start with an @ symbol")
 	}
 
@@ -42,6 +49,6 @@ func (p *Person) SetTwitterHandler(handler string) error {
 	return nil
 }
 
-func (p *Person) TwitterHandler() string {
+func (p *Person) TwitterHandler() TwitterHandler {
 	return p.twitterHandler
 }
